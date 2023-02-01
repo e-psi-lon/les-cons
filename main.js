97c1f1cbe47f82eced6e0cfe4ab02d806d065767
e_ψ_lon <79647119+e-psi-lon@users.noreply.github.com>
2023-02-01 15:24:08 +0100

    Je suis aussi con que ce site

--- main.js
+++ main.js
@@ -3,11 +3,7 @@ function con() {
     while (!(100<=clickcount && clickcount<=1000)) {
         clickcount = prompt(`Choisis un nombre entre 100 et 1000 pas ${clickcount}`)
     }
-    click()
-
-    function click() {
-        for (let i = 0; i < clickcount; i++) {
-            alert(`Maintenant tu va cliquer comme un con encore ${clickcount - i} fois`)
-        }
+    for (let i = 0; i < clickcount; i++) {
+        alert(`Maintenant tu va cliquer comme un con encore ${clickcount - i} fois`)
     }
 }
