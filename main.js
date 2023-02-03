82c379b26bdda5629f9f7dd235638eb091c135c0
e_ψ_lon <79647119+e-psi-lon@users.noreply.github.com>
2023-02-03 15:52:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ function con() {
     for (let i = 0; i < clickcount; i++) {
         alert(`Maintenant tu va cliquer comme un con encore ${clickcount - i} fois`)
     }
-    document.getElementById("image").innerHTML = "<img href=\"bon-toutou.webp\">";
+    document.getElementById("image").innerHTML = "<img src=\"bon-toutou.webp\">";
 }
